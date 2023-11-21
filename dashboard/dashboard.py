ba4c74ba1039de138200d8e9e688a187f922608c
karna0811-bd <karna.mandara@gmail.com>
2023-11-21 10:27:34 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,8 +1,12 @@
-import streamlit as st 
- 
-st.write(
-    """
-    # My first app
-    Hello, para calon praktisi data masa depan!
-    """
-)
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+by_month_df = pd.read_csv("dashboard/most_sold_items_by_month.csv")
+#by_month_df.head()
+st.write(pd.DataFrame({
+    by_month_df
+}))
